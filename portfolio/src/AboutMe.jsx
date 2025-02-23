5981fd34f4021326052118ed5703e76d165d3b14
Bilo-rm <bilal2002mahfouth@gmail.com>
2025-02-23 20:51:01 +0200

    adding Taillwind css for styling

--- portfolio/src/AboutMe.jsx
+++ portfolio/src/AboutMe.jsx
@@ -5,7 +5,7 @@ function AboutMe() {
   return (
     <>
 
-      <h1>Bilal Mahfouth</h1>
+      <h1 className="text-3xl font-bold underline bg-red-500">Bilal Mahfouth</h1>
       <p>
       Motivated and results-driven Junior Software Engineer with hands-on experience in designing, developing, and maintaining scalable, high-performance web applications. Proficient in modern technologies, including React.js, Express, and PostgreSQL, with a strong focus on building responsive and user-friendly interfaces. Skilled in collaborating with cross-functional teams to deliver innovative solutions and meet project goals effectively. Adept at back-end development, API design, and database management, with foundational knowledge of AI concepts and practices. Eager to continuously enhance technical expertise, embrace challenges, and contribute to impactful projects in a fast-paced environment.
       </p>
